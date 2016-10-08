341df37b3f228187f73a22ec69c8348c35362e56
Alex Lane <amlane86@yahoo.com>
2016-10-08 10:14:18 -0400

    Created send mail action and view

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -8,5 +8,15 @@ use App\Http\Requests;
 
 class EmailController extends Controller
 {
-    //
+
+    public function send(Request $request){
+        $title = $request->input('title');
+        $content = $request->input('content');
+
+        Mail::send('emails.send', ['title' => $title, 'content' => $content], function($message) {
+            $message->to('amlane86@yahoo.com');
+        });
+
+        return response()->json(['message' => 'Request completed']);
+    }
 }
