a66bc9e6546550cf121aa86119b63ef96b0ae3fd
Alex Lane <amlane86@yahoo.com>
2016-10-08 16:31:15 -0400

    Adden token authentication for api methods that don't require login

---
+++ app/Http/Middleware/RoeReportsVerifyCsrf.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexander
+ * Date: 10/8/2016
+ * Time: 10:49 AM
+ */
+
+namespace App\Http\Middleware;
+
+class RoeReportsVerifyCsrf extends \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken {
+    /**
+     * Handle the incoming request
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Session\TokenMismatchException
+     */
+    public function handle($request, Closure $next){
+        if($this->isReading($request) || $this->isAuthExcludedRoutes($request) || $this->tokensMatch($request)){
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        throw new TokenMismatchException;
+    }
+
+    /**
+     * This will return a bool value bases on the route checking and APP_API_KEY nad APP_API_SECRET
+     *
+     * @param Request $request
+     * @return boolean
+     *
+     * @throws unauthenticated
+     */
+    protected function isAuthExcludedRoutes($request){
+        /*
+         *
+         * STILL NEED TO ADD AUTHENICATION WITH API KEY AND SECRET
+         * ALSO NEED TO SET ROUTES TO THE TOKEN EXCLUDE FILE INSTEAD OF HARDCODED HERE
+         */
+
+        $routes = include '../../../routes/tokenexclude.php';
+
+        foreach($routes as $route){
+            if($request->is($route) && base64_decode($request->input('token') === env('APP_API_SECRET'))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
