4e62953178c156cc79cff55c8ec12455fff49e7b
Alex Lane <amlane86@yahoo.com>
2016-10-11 18:16:23 -0400

    Adden token authentication for api methods that don't require login

--- app/Http/Middleware/RoeReportsVerifyCsrf.php
+++ app/Http/Middleware/RoeReportsVerifyCsrf.php
@@ -8,54 +8,6 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Session\TokenMismatchException;
-
 class RoeReportsVerifyCsrf extends \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken {
-    /**
-     * Handle the incoming request
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
-     * @return mixed
-     *
-     * @throws \Illuminate\Session\TokenMismatchException
-     */
-    public function handle($request, Closure $next){
-        if($this->isAuthExcludedRoutes($request)){
-            return $this->addCookieToResponse($request, $next($request));
-        }
-
-        if($this->isReading($request) || $this->tokensMatch($request)){
-            return $this->addCookieToResponse($request, $next($request));
-        }
-
-        throw new TokenMismatchException;
-    }
-
-    /**
-     * This will return a bool value bases on the route checking and APP_API_KEY nad APP_API_SECRET
-     *
-     * @param Request $request
-     * @return boolean
-     *
-     * @throws unauthenticated
-     */
-    protected function isAuthExcludedRoutes($request){
-        /*
-         *
-         * STILL NEED TO ADD AUTHENICATION WITH API KEY AND SECRET
-         * ALSO NEED TO SET ROUTES TO THE TOKEN EXCLUDE FILE INSTEAD OF HARDCODED HERE
-         */
-
-        $routes = ['/send']; //include '../../../routes/tokenexclude.php';
-
-        foreach($routes as $route){
-            if($request->is($route)){ //&& base64_decode($request->input('token') === env('APP_API_SECRET'))){
-                return true;
-            }
-        }
-
-        return false;
-    }
+   protected $except = ['/send'];
 }
