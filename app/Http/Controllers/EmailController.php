ae1e7434ddc6d66c78abe07b6b238877026ef02b
Alex Lane <amlane86@yahoo.com>
2016-10-12 22:39:32 -0400

    Adden token authentication for api methods that don't require login

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use Mail;
-use Illuminate\Http\Request;
+use Request;
 
 class EmailController extends Controller
 {
 
     public function send(Request $request){
         $data = array(
-            'title' => Input::get('title'),
-            'content' => Input::get('content')
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
         );
 
-        $to = Input::get('email');
+        $to = $request->input('email');
 
         Mail::send('emails.send', $data, function($message) use ($to){
             $message->to($to);
