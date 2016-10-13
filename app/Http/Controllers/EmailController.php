c1818f37a64176e095bdd292687a99b6be09f6b7
Alex Lane <amlane86@yahoo.com>
2016-10-13 19:06:33 -0400

    Adden token authentication for api methods that don't require login

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class EmailController extends Controller
 {
 
-    public function send(Request $request){
+    /**
+     * Welcome email after registration
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function welcome(Request $request){
         $data = array(
             'title' => $request->input('title'),
             'content' => $request->input('content')
@@ -16,10 +22,44 @@ class EmailController extends Controller
 
         $to = $request->input('email');
 
-        Mail::send('emails.send', $data, function($message) use ($to){
-            $message->to($to);
-        });
+        return response()->json(['keys' => $request->all()]);
+        //return $this->send('welcome', $to, $data);
+    }
+
+    /**
+     * Send and email to the user
+     *
+     * @param $layout
+     * @param $to
+     * @param $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function send($layout, $to, $data){
+        try {
+            Mail::send('emails.' . $layout, $data, function($message) use ($layout, $to){
+                $message->to($to)->subject($this->getSubject($layout));
+            });
+        } catch(Exception $ex) {
+            return response()->json(['message' => $ex->getMessage()]);
+        }
 
         return response()->json(['message' => 'Request completed']);
+    }
+
+    /**
+     * Returns the email subject based on the type of notification
+     *
+     * @param $layout
+     * @return string
+     */
+    private function getSubject($layout){
+        switch($layout){
+            case 'welcome':
+                return 'Welcome to ' . env('APP_NAME') . '!';
+                break;
+            default:
+                return 'Notification from ' . env('APP_NAME');
+                break;
+        }
     }
 }
