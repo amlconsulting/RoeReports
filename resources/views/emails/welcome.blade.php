94a71b84ff78d0fcaaa9c94651713b477c1338d9
Alex Lane <amlane86@yahoo.com>
2016-10-13 19:30:58 -0400

    Adden token authentication for api methods that don't require login

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up Confirmation</title>
+</head>
+<body>
+<h1>Welcome to {{ env('APP_NAME') }}!</h1>
+
+<p>
+    We are happy you are here.
+</p>
+</body>
+</html>
