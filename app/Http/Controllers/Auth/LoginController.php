6c007ad31a17be361daa9c1cafe407b1d91f709c
Alex Lane <amlane86@yahoo.com>
2016-10-22 19:41:23 -0400

    Added message to login screen if the user is not verified.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -39,13 +40,49 @@ class LoginController extends Controller
     }
 
     /**
+     * Log the user in
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function login(Request $request) {
+        $user = User::whereEmail($request->input('email'))->firstOrFail();
+        if(!$user->verified) {
+            flash('Your email has not been confirmed yet.', 'warning');
+            return redirect()->back();
+        }
+
+        $this->validateLogin($request);
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        $credentials = $this->credentials($request);
+
+        if ($this->guard()->attempt($credentials, $request->has('remember'))) {
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function postLogin(Request $request) {
+
+    }
+
+    /**
      * Get the needed authorization credentials from the request.
      *
      * @param Request $request
      * @return mixed
      */
-    protected function credentials(Request $request) {
+    /*protected function credentials(Request $request) {
         return $request->only($this->username(), 'password') + ['verified' => true];
-    }
+    }*/
 
 }
